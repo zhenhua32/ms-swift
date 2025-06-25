ed8b0a0c3da6df23bdfb3395c7e8406511199c2c
zhenhua32 <zhenhua32@gmail.com>
2025-06-25 22:23:34 +0800

    会卡住

---
+++ notebook/train_grpo.sh
@@ -0,0 +1,44 @@
+CUDA_VISIBLE_DEVICES=0 \
+NPROC_PER_NODE=1 \
+swift rlhf \
+    --rlhf_type grpo \
+    --model /mnt/workspace/model/Qwen3-0.6B \
+    --train_type lora \
+    --use_vllm true \
+    --vllm_mode colocate \
+    --vllm_gpu_memory_utilization 0.5 \
+    --vllm_max_model_len 2048 \
+    --vllm_tensor_parallel_size 1 \
+    --dataset AI-MO/NuminaMath-TIR#10000 \
+    --torch_dtype bfloat16 \
+    --num_train_epochs 1 \
+    --max_length 2048 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps 1 \
+    --eval_steps 100 \
+    --save_steps 100 \
+    --learning_rate 1e-6 \
+    --save_total_limit 2 \
+    --logging_steps 5 \
+    --output_dir output \
+    --warmup_ratio 0.05 \
+    --dataloader_num_workers 4 \
+    --max_completion_length 1024 \
+    --reward_funcs accuracy format \
+    --reward_model /mnt/workspace/model/Qwen3-4B \
+    --reward_model_plugin genrm \
+    --reward_weights 0.5 0.5 1 \
+    --num_generations 2 \
+    --system /mnt/workspace/ms-swift-3.5.2/examples/train/grpo/prompt.txt \
+    --deepspeed zero3 \
+    --temperature 1.0 \
+    --top_p 1.0 \
+    --top_k 80 \
+    --log_completions true \
+    --async_generate false \
+    --move_model_batches 16 \
+    --offload_optimizer true \
+    --offload_model true \
+    --gc_collect_after_offload true \
+    --sleep_level 0
